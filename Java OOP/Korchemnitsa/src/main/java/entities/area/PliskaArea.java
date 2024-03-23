b616d56cf414dac15544eeb1fd9c730fde3cf7d6
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-23 11:36:01 +0200

    Create PliskaArea.java

---
+++ Java OOP/Korchemnitsa/src/main/java/entities/area/PliskaArea.java
@@ -0,0 +1,9 @@
+package entities.area;
+
+public class PliskaArea extends BaseArea {
+    private static final int TABLE_CAPACITY = 10;
+
+    public PliskaArea(String name) {
+        super(name, TABLE_CAPACITY);
+    }
+}
