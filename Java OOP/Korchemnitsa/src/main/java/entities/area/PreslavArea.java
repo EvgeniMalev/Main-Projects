e30bfbce619ac02c22a290c31762e047817e6a9b
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-23 11:36:50 +0200

    Create PreslavArea.java

---
+++ Java OOP/Korchemnitsa/src/main/java/entities/area/PreslavArea.java
@@ -0,0 +1,9 @@
+package entities.area;
+
+public class PreslavArea extends BaseArea {
+    private static final int TABLE_CAPACITY = 15;
+
+    public PreslavArea(String name) {
+        super(name, TABLE_CAPACITY);
+    }
+}
