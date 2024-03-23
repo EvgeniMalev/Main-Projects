c05620e976d1c3ad38431a4410b9dcbd98a0aa16
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-03-23 11:40:11 +0200

    Create OchridArea.java

---
+++ Java OOP/Korchemnitsa/src/main/java/entities/area/OchridArea.java
@@ -0,0 +1,9 @@
+package entities.area;
+
+public class OchridArea extends BaseArea {
+    private static final int TABLE_CAPACITY = 8;
+
+    public OchridArea(String name) {
+        super(name, TABLE_CAPACITY);
+    }
+}
